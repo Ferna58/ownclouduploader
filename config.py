5174a9c66a80343fe403a5554957a8845f72a888
Obisoft Dev <104738729+ObisoftDev@users.noreply.github.com>
2022-11-05 10:04:12 -0500

    Update config.py

--- config.py
+++ config.py
@@ -10,15 +10,12 @@ BASE_ROOT_PATH = 'root/'
 #Account Config
 OWN_USER = os.environ.get('account_user')
 OWN_PASSWORD = os.environ.get('account_password')
+OWN_HOST = os.environ.get('host','https://misarchivos.uci.cu/owncloud/')
 
 # Proxy Config
 PROXY_OBJ = ProxyCloud.parse(os.environ.get('proxy_enc'))
 #PROXY_OBJ = ProxyCloud.parse('socks5://KHDEKJYEJJJKGIYDJHGFGEYHKKFJEGRIDHLIDILD')
 
-#SET In DEBUG
-#BOT_TOKEN = '5650727294:AAE4-x3fzPWLeIopQ9b4PWjOYza_U8shJRE'
-#OWN_USER = 'ljgaliano'
-#OWN_PASSWORD = 'Pelusa1234/**'
 
 if PROXY_OBJ:
     print(f"PROXY :{PROXY_OBJ.ip}:{PROXY_OBJ.port}")
