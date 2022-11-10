b84323d8cdd04c7b2357ac03585b18f13f2e40d7
Obisoft Dev <104738729+ObisoftDev@users.noreply.github.com>
2022-11-10 09:04:54 -0500

    Update downloader.py

--- pydownloader/downloader.py
+++ pydownloader/downloader.py
@@ -2,7 +2,6 @@ import time
 import os
 import re
 import requests
-from . import youtube
 from . import googledrive
 from . import mediafire
 from .utils import req_file_size,get_file_size,get_url_file_name,slugify,createID,makeSafeFilename
@@ -26,15 +25,7 @@ class Downloader(object):
         self.progressfunc = progressfunc
         self.args = args
         req = None
-        if 'youtube' in url or 'youtu.be' in url:
-                try:
-                    data = youtube.getVideoData(url)
-                    if data:
-                        url = data['url']
-                        self.filename = slugify(data['name'])
-                    else: return None
-                except: return None
-        elif 'mediafire' in url:
+        if 'mediafire' in url:
                 try:
                     url = mediafire.get(url)
                 except:return None
